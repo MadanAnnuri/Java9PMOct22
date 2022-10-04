dbb04bef990f3efc8a753c773cef01ccae88b03c
MadanAnnuri <annurimadan@gmail.com>
2022-10-04 17:58:24 +0530

    updated practice class with multiplication

--- src/Practice1.java
+++ src/Practice1.java
@@ -1,9 +1,11 @@
 public class Practice1 {
     public static void main(String[] args) {
-        System.out.println();
+       // System.out.println();
         int a=1;
         int b=12;
         System.out.println(a>b);
         System.out.println(a+b); // addition
+        int c= a*b;
+        System.out.println(c);//multiplication
     }
 }
