c8aef791f03bc212f80698057fcecbfa40e753db
MadanAnnuri <annurimadan@gmail.com>
2022-10-04 17:53:26 +0530

    updated practice class

---
+++ src/Practice1.java
@@ -0,0 +1,9 @@
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println();
+        int a=1;
+        int b=12;
+        System.out.println(a>b);
+        System.out.println(a+b); // addition
+    }
+}
